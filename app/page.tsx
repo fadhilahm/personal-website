49ff41c71331eb0b82abe78a015620c434497df6
fadhilahm <fadhilahmetra@gmail.com>
2023-07-20 23:02:02 +0900

    🔥 chore(.env.example): remove unused environment variable examples
    🔥 chore(.eslintrc.cjs): remove ESLint configuration file
    
    ✨ feat(.eslintrc.json): add ESLint configuration file with Next.js core-web-vitals plugin
    
    🔧 chore(.gitignore): update .gitignore to exclude unnecessary files
    
    🆕 feat(README.md): update README.md with Next.js project information
    
    🆕 feat(app/favicon.ico): add favicon.ico file
    
    🆕 feat(app/globals.css): add global CSS file for styling
    
    🆕 feat(app/layout.tsx): add layout component with global CSS import
    
    🆕 feat(app/page.tsx): add home page component with sample content
    
    🆕 feat(next.config.js): add empty Next.js configuration file
    
    🔥 chore(next.config.mjs): remove Next.js configuration file
    
    🔧 chore(package.json): update dependencies and scripts
    
    🔧 chore(postcss.config.js): add PostCSS configuration file
    
    🔥 chore(prettier.config.cjs): remove Prettier configuration file
    
    🆕 feat(public/favicon.ico): add favicon.ico file
    
    🆕 feat(public/next.svg): add Next.js logo SVG file
    
    🆕 feat(public/vercel.svg): add Vercel logo SVG file
    
    🔥 chore(src/env.mjs): remove unused environment variables file
    
    🔥 chore(src/pages/_app.tsx): remove unused _app.tsx file
    
    🔥 chore(src/pages/index.tsx): remove unused index.tsx file
    
    🔥 chore(src/styles/globals.css): remove unused global CSS file
    
    🆕 feat(tailwind.config.js): add Tailwind CSS configuration file
    
    🔥 chore(tailwind.config.ts): remove unused Tailwind CSS configuration file
    
    🔧 chore(tsconfig.json): update TypeScript configuration file

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-red-700">
+      <span className='text-[5rem] font-extrabold rotate-[-5deg]'>ハローハッピーワールド！！！</span>
+    </main>
+  )
+}
