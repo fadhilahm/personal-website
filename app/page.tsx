862dda9150eeb35c7f571bb70b90f9c219a726cb
fadhilahm <fadhilahmetra@gmail.com>
2024-06-05 18:38:45 +0900

    test auto deployment

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-red-700">
-      <span className='text-[5rem] font-extrabold rotate-[-5deg]'>ハローハッピーワールド！！！</span>
+      <span className='text-[5rem] font-extrabold rotate-[-5deg]'>ハローハッピーワールド2！！！</span>
     </main>
   )
 }
